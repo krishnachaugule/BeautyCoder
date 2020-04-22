51e29ff8437db0014eb5ceb5be9d3f43194aeb8c
krishnachaugule <58576563+krishnachaugule@users.noreply.github.com>
2020-04-21 21:54:31 -0400

    created a separate js

--- Scripts/puzzle1.js
+++ Scripts/puzzle1.js
@@ -1,1 +1,22 @@
 // JavaScript source code
+    function mySum() {
+
+            var x = document.getElementById("S1").value;
+            var y = document.getElementById("S2").value;
+            var z = document.getElementById("S3").value;
+            var a = document.getElementById("S4").value;
+            if (x == 1.5 && y == 12.5 && z == 13.5 && a == 3.5) {
+        alert("Congratulations! You got that right!")
+    }
+            else if (x == 1.5 && y == 12.5 && z == 13.5 || x == 1.5 && z == 13.5 && a == 3.5 || y == 12.5 && z == 13.5 && a == 3.5 || z == 13.5 && a == 3.5 && x == 1.5 || z == 13.5 && a == 3.5 && y == 12.5 || y == 12.5 && z == 13.5 && a == 3.5 || z == 13.5 && a == 3.5 && x == 1.5) {
+        alert("Almost There!! Try again!!")
+    }
+            else if (x == 1.5 && y == 12.5 || x == 1.5 && z == 13.5 || x == 1.5 && a == 3.5 || y == 12.5 && z == 13.5 || y == 12.5 && a == 3.5 || z == 13.5 && a == 3.5 || z == 13.5 && y == 12.5)
+            //  z == 13.5 && x == 1.5 ||
+            {
+        alert("Getting There!! Try sagin!!")
+    }
+            else {
+        alert("Try again!!")
+    }
+        }
