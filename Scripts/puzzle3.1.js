3ebbc68c46ae990f4692f81052fef7470a4e7795
krishnachaugule <58576563+krishnachaugule@users.noreply.github.com>
2020-05-11 12:32:48 -0400

    3.1

---
+++ Scripts/puzzle3.1.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+function myAnswer4() {
+    var a = document.getElementById("A1").value;
+    if (a == 86) {
+        alert("Congratulations! You got that right!")
+    }
+    else {
+        alert("Don't Give Up!! Try Again!!")
+    }
+}
+function myHint() {
+    //Each clock, calculator and bulb has its own significance that helps derive its value!
+    alert("The fate of the world is in your hands!")
+}// JavaScript source code
