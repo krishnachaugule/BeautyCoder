24daa58a8d1531d3f92cc1e2a9b4f1c8373362e5
krishnachaugule <58576563+krishnachaugule@users.noreply.github.com>
2020-05-20 17:22:56 -0400

    3 new levels

---
+++ Scripts/puzzle0_1.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+function myAnswer5() {
+    var a = document.getElementById("A1").value;
+    if (a == 30) {
+        alert("Congratulations! You got that right!")
+    }
+    else {
+        alert("Don't Give Up!! Try Again!!")
+    }
+}
+function myHint() {
+    alert("Try changing the numbers up a bit.")
+}
---
+++ Scripts/puzzle0_2.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+function myAnswer5() {
+    var a = document.getElementById("A1").value;
+    if (a == 96) {
+        alert("Congratulations! You got that right!")
+    }
+    else {
+        alert("Don't Give Up!! Try Again!!")
+    }
+}
+function myHint() {
+    alert("Try changing the numbers and signs up a bit.")
+}
